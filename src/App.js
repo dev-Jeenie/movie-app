b7668baf5c486206ab6f0cb7944fb59fc7e5a15c
jeenie <devjeenie@gmail.com>
2021-09-27 04:19:50 +0900

    review

--- src/App.js
+++ src/App.js
@@ -1,44 +1,51 @@
 import React from 'react';
  
 
-function Food({ name, picture }){
+function Food({ name, picture }) {
+  // props.name이나 {name}이나 같은거다
+  // console.log(props);
   return (
-  <div>
-    <h1>I like {name}</h1>
-    <img src={picture}/>
+    <div>
+      <h1>I like {name}</h1>
+      <img src={picture} />
     </div>
   );
 }
- 
+
 const foodILike = [
   {
-  name: "Kimchi",
-  image: "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg"
+    name: "Kimchi",
+    image:
+      "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
   },
   {
-  name: "bulgogi",
-  image: "https://www.google.com/search?q=%EB%B6%88%EA%B3%A0%EA%B8%B0&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk02ziUIXmsNvR7TuYKh1cpJGpdpH8g:1593509047151&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi-p87lm6nqAhVbUd4KHVSWBBUQ_AUoAXoECBgQAw&biw=1440&bih=701&dpr=2#imgrc=kjZoAmvHGo-KfM"
+    name: "bulgogi",
+    image:
+      "https://www.google.com/search?q=%EB%B6%88%EA%B3%A0%EA%B8%B0&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk02ziUIXmsNvR7TuYKh1cpJGpdpH8g:1593509047151&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi-p87lm6nqAhVbUd4KHVSWBBUQ_AUoAXoECBgQAw&biw=1440&bih=701&dpr=2#imgrc=kjZoAmvHGo-KfM",
   },
   {
-  name: "kimbap",
-  image: "https://www.google.com/search?q=%EA%B9%80%EB%B0%A5&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk03dn_nrK6M5JlKKfD5krm8w3qTmGQ:1593509069629&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjqn6rwm6nqAhXDQN4KHYLUA4EQ_AUoAXoECBgQAw&biw=1440&bih=701#imgrc=Xz3xQoYZw3bqEM"
+    name: "kimbap",
+    image:
+      "https://www.google.com/search?q=%EA%B9%80%EB%B0%A5&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk03dn_nrK6M5JlKKfD5krm8w3qTmGQ:1593509069629&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjqn6rwm6nqAhXDQN4KHYLUA4EQ_AUoAXoECBgQAw&biw=1440&bih=701#imgrc=Xz3xQoYZw3bqEM",
   },
   {
-  name: "samgyetang",
-  image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg"
-  }
-]
-
+    name: "samgyetang",
+    image:
+      "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg",
+  },
+];
 
 function App() {
-  return(
+  return (
     <div>
       <h1>Hello</h1>
-      {foodILike.map(dish => <Food name={dish.name} picture={dish.image}/>)}
+      {foodILike.map((dish) => (
+        <Food name={dish.name} picture={dish.image} />
+      ))}
       {/* 여기서 dish는 객체다. 마우스 올려보면 name이랑 image가 나타난다 */}
+      {/* Food 컴포넌트에 fav라는 이름의 property를 kimchi라는 value로 준것이다 */}
     </div>
-  ); 
-
+  );
 }
 
 export default App;
