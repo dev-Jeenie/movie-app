a555c59a53e1988979bc558e7c8fb443c8dea164
jeenie <jeenie@actbase.io>
2021-09-29 04:12:13 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,68 +1,45 @@
 import React from "react";
 import PropTypes from "prop-types";
+import axios from "axios";
 
 class App extends React.Component {
-  // reactcomponent에서 확장된 App
-  /** camera,screen,charger를 cell phone class에 넣은 다음
-   * 그 cell phone class에서 확장된 samsung class와 apple class
-   * 그럼 samsung class는 cell phone class에서 모든 것을 가져올 수 있다
-   * 통틀어서 추상화한 거라고 생각(밀가루 붓는 틀 -> 빵틀 -> 붕어빵틀, 와플틀, 계란빵틀...) */
-  constructor(props) {
-    /** constructor는 render의 전에 호출된다.
-     *    컴포넌트가 mount될 때, 컴포넌트가 screen에 표시될 때, 컴포넌트가 웹사이트에 갈 때 constructor를 호출한다
-     */
-    super(props);
-    console.log("hello");
-  }
+  /**
+   * 순서
+   * 1. isLoading: true이기 때문에 Loading이 랜더
+   * 2. application이 마운트(태어남)되자마자 componentDidMount, getMovies를 호출
+   * 3. getMovies는 axios.get을 사용하지만 시간이 필요해서 await를 넣음.
+   *  */
   state = {
-    count: 0,
+    isLoading: true,
+    movies: [],
+    // 이곳에 쓰는 future state는 필수가 아님
   };
-  add = () => {
-    /*
-    this.state.count = 1; (X)
-    //Do not mutate state directly. Use setState().
-    그리고 이렇게 하면 react가 render function을 refresh하지 않는다
-    매번 state의 상태를 변경할 때마다, react가 render function을 호출해서 바꾸게 해야한다
-    => state를 변경하고, 바뀔 때마다 react가 refresh 해줘야하니까 setState를 쓴다!
-    setState를 할 때마다, react는 새로운 state와 함께 render function을 호출한다!!!!!!!!
-    */
-    this.setState((current) => ({ count: current.count + 1 }));
-    // this.setState(() => ({ count: this.state.count + 1 })); state(외부)에 너무 의존하기때문에 좋지않음
-    // 그래서 function 방식으로 현재의 state를 가져올 수 있음
+  getMovies = async () => {
+    const movies = await axios.get("https://yts.mx/api/v2/list_movies.json");
+    console.log(movies.data.data.movies);
+    /** axios.get는 시간이 걸린다. 기다리라고 전해줘야함.
+     * async = "너는 비동기야 = 넌 기다려야해"
+     * 뭐를 기다려?
+     * await axios.get = axios를 기다려
+     * => await 끝날때까지 기다렸다가 계속해라!
+     */
   };
-  minus = () => {
-    this.setState((current) => ({ count: current.count - 1 }));
-  };
-  /** Component의 Life cycle */
   componentDidMount() {
-    // 컴포넌트가 처음 render된건지 알려줌
-    console.log("component rendered ");
+    this.getMovies();
+    // setTimeout(() => {
+    //   this.setState({ isLoading: false, book: true });
+    //   // state에 있는 것들이 필수가 아니기때문에 book처럼 state를 추가해도 괜찮다
+    // }, 2000);
   }
-  componentDidUpdate() {
-    console.log("I just updated ");
-  }
-  componentWillUnmount() {
-    console.log("Goodbye, cruel world");
-  }
-
   render() {
-    console.log("I am rendering");
-    // react는 자동적으로 class component의 render method를 실행한다!
-    return (
-      <div>
-        <h1> The number is : {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>minus</button>
-      </div>
-    );
-    /** 이 render method는 react.component가 가지고 있었지만, 확장했기 때문에 App component도 가지게 되었다 */
+    const { isLoading } = this.state;
+    // return <div>{this.state.isLoading ? "Loading" : "We are ready"}</div>;
+    // this.state.isLoading을 다 쓰지 않고
+    return <div>{isLoading ? "Loading......" : "We are ready"}</div>;
   }
 }
 
 export default App;
 
-/**Function Component와 Class Component의 차이점
- *  Function Component는 function이고, 뭔가를 return한 뒤 screen에 표시됨
- *  Class Component는 class이고, react component로부터 확장되어서 screen에 표시됨 -> 이걸 render method에 넣어야함
- *
- */
+// https://yts.mx/api
+// https://yts.mx/api/v2/list_movies.json 로 (니꼬가 만든것)
