73dd011c6066a1f268677fd7dcf4781e2663bdc9
jeenie <jeenie@actbase.io>
2021-09-29 02:05:57 +0900

    add propTypes

--- src/App.js
+++ src/App.js
@@ -1,46 +1,66 @@
-import React from 'react';
- 
-
-function Food({ name, picture }) {
-  // props.name이나 {name}이나 같은거다
-  // console.log(props);
-  return (
-    <div>
-      <h1>I like {name}</h1>
-      <img src={picture} />
-    </div>
-  );
-}
+import React from "react";
+import PropTypes from "prop-types";
 
 const foodILike = [
   {
+    id: 1,
     name: "Kimchi",
     image:
       "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
+    // rating: 5,
   },
   {
+    id: 2,
     name: "bulgogi",
     image:
       "https://www.google.com/search?q=%EB%B6%88%EA%B3%A0%EA%B8%B0&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk02ziUIXmsNvR7TuYKh1cpJGpdpH8g:1593509047151&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi-p87lm6nqAhVbUd4KHVSWBBUQ_AUoAXoECBgQAw&biw=1440&bih=701&dpr=2#imgrc=kjZoAmvHGo-KfM",
+    rating: 3,
   },
   {
+    id: 3,
     name: "kimbap",
     image:
       "https://www.google.com/search?q=%EA%B9%80%EB%B0%A5&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk03dn_nrK6M5JlKKfD5krm8w3qTmGQ:1593509069629&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjqn6rwm6nqAhXDQN4KHYLUA4EQ_AUoAXoECBgQAw&biw=1440&bih=701#imgrc=Xz3xQoYZw3bqEM",
+    rating: 4.9,
   },
   {
+    id: 4,
     name: "samgyetang",
     image:
       "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg",
+    rating: 3,
   },
 ];
+
+function Food({ name, picture, rating }) {
+  // props.name이나 {name}이나 같은거다
+  return (
+    <div>
+      <h1>I like {name}</h1>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name} />
+    </div>
+  );
+}
+Food.propTypes = {
+  // propTypes의 이름은 반드시 propTypes 여야함
+  // https://ko.reactjs.org/docs/typechecking-with-proptypes.html 문서 참조.
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+};
 
 function App() {
   return (
     <div>
       <h1>Hello</h1>
       {foodILike.map((dish) => (
-        <Food name={dish.name} picture={dish.image} />
+        <Food
+          key={dish.id}
+          name={dish.name}
+          picture={dish.image}
+          rating={dish.rating}
+        />
       ))}
       {/* 여기서 dish는 객체다. 마우스 올려보면 name이랑 image가 나타난다 */}
       {/* Food 컴포넌트에 fav라는 이름의 property를 kimchi라는 value로 준것이다 */}
