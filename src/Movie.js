105f58a94c6a374be731b387dcfd787039909194
jeenie <jeenie@actbase.io>
2021-10-05 23:59:59 +0900

    d

--- src/Movie.js
+++ src/Movie.js
@@ -9,7 +9,7 @@ function Movie({ id, year, title, summary, poster, genres }) {
       <img src={poster} alt={title} title={title} />
       <div className="movie_data">
         <h3 className="movie_title">{title}</h3>
-        <h5 className="movie_year">{year}</h5>
+        <h5 className="movie_year   ">{year}</h5>
         <p className="movie_summary">{summary.slice(0, 180)}...</p>
         <ul className="genres">
           {genres.map((genre, index) => (
